c6f43d416d58c75bb1282c2184be89a7435e8b0b
nomi_aziz <n.aziz@reply.it>
2021-11-27 12:22:19 +0100

    test

---
+++ AnagVehicle/test.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+//dimostazione
+// define the home page route
+router.get('/', function(req, res) {
+  res.send('Birds home page');
+});
+// define the about route
+router.get('/about', function(req, res) {
+  res.send('About birds');
+});
+
+module.exports = router;
